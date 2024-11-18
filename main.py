f874012db1344fdfc88a90352aa83212cd977b32
ianic-dev <ian@ianic.dev>
2024-11-18 15:39:32 +0100

    added basic load data structure

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('hm, is the setup done correctly?') # ls
+    print_hi('hm, is the setup done correctly?')  # ls
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
