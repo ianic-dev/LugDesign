3c449c853afaeb2bce84a12628fc1edf69ec2947
Kamil <jakubowski.6.kamil@gmail.com>
2024-11-20 17:31:54 +0100

    WIP on computing xz forces

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-from data_ingress import LoadCase, MaterialProperties, LugConfig, read_lugconfig_from_csv
+from data_ingress import LoadCase, MaterialProperties, LugConfig, read_lugconfig_from_csv, BackplatePins
 
 def print_hi(name):
     print(f'Hi, {name}')
 
 
 if __name__ == '__main__':
-    test_lugconfig = LugConfig(1, 1, 1, 1, 1, 1, 1)
+    test_lugconfig = LugConfig(1, 2, 1, 1, 1, 1, 1)
+    forces = LoadCase(1,1,1,1,1,1)
     test_lugconfig.to_csv("test")
     read_lugconfig_from_csv("test")
+    pos_holes=[[3,1],[2,2],[-1,-1],[-2,-2]]
+    backplate=BackplatePins(4,pos_holes)
+    cg=backplate.compute_cg(test_lugconfig)
+    
+print(backplate.compute_xz_hole_force(cg,test_lugconfig,forces))
