969c1f452e6220eeaacdf84a74de3a9b47aa3067
Kamil <jakubowski.6.kamil@gmail.com>
2024-11-25 17:34:40 +0100

    Added y forces and pull and bearing checks

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
-from data_ingress import LoadCase, MaterialProperties, LugConfig, BackplatePins
+from data_ingress import LoadCase, MaterialProperties, LugConfig
+from backplate import BackplatePins
 
 
 def print_hi(name):
@@ -8,10 +9,12 @@ def print_hi(name):
 
 if __name__ == '__main__':
     test_lugconfig = LugConfig(1, 2, 1, 1, 1, 1, 1)
-    forces = LoadCase(1,1,1,1,1,1)
-    pos_holes=[[2,1],[1,-1],[-1,-1]]
-    backplate=BackplatePins(3,pos_holes)
+    forces = LoadCase(0,0,-44.818,1,153.036,0)
+    pos_holes=[[0.1,0.1],[0.1,-0.1],[-0.1,0.1],[-0.1,-0.1]]
+    backplate=BackplatePins(4,pos_holes)
     cg=backplate.compute_cg(test_lugconfig)
     
 print(cg)
 print(backplate.compute_xz_hole_force(cg,test_lugconfig,forces))
+print(backplate.compute_y_hole_force(cg,test_lugconfig,forces))
+print(backplate.pull_out_check(backplate.compute_y_hole_force(cg,test_lugconfig,forces),0.005,0.005,276000000))
